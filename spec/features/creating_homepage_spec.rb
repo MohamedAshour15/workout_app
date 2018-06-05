fe6301161c571244e8d293c2337d7e1656a0a218
mohamed.ashour <mohamed.ashour@pharos-solutions.de>
2018-06-05 09:30:55 -0400

    Hide Links

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -1,8 +1,11 @@
  require 'rails_helper'
 RSpec.feature "Creating Home Page" do
+  before do
+    @john=User.create(:email "john@example.com",password: "password")
+  end
   scenario do
-  visit '/'
     
+  visit '/'
   expect(page).to have_content('Workout Lounge!')
   expect(page).to have_content('Show off your workout')
    expect(page).to have_link('Home')
