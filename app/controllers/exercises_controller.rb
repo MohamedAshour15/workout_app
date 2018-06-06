ffbadf1211b89ec4c8bfa1cc2efa32f2ae281d31
mohamed.ashour <mohamed.ashour@pharos-solutions.de>
2018-06-06 06:57:22 -0400

    Implement create show actions and show templates

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,7 +5,28 @@ class ExercisesController < ApplicationController
     def new
     @exercise=current_user.exercises.new
   end
+  
   def create
-    
+    @exercise=current_user.exercises.new(exercise_params)
+    if @exercise.save
+      flash[:notice]="Exercise has been created"
+      redirect_to user_exercise_path(current_user,@exercise)
+      # or redirect_to [current_user,@exercise]
+    else
+      flash[:alert]="Exercise hasn't been created"
+      render new
+    end
   end
+  
+  def show
+    @exercise=current_user.exercises.find params[:id]
+  end
+  
+  
+  private 
+  
+  def exercise_params
+  params.require(:exercise).permit(:duration_in_min,:workout,:workout_date,:user_id)  
+  end
+ 
 end
